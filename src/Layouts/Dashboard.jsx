b17bc2e1a5c748aca7e3349696c240e6b8f9de45
tonmoybosunia24 <mdtonmoybosunia24@gmail.com>
2025-02-09 15:22:24 +0600

    Dashboard Card Items Page Complete

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { FaCalendarAlt, FaShoppingCart } from "react-icons/fa";
 import { FaBagShopping, FaWallet } from "react-icons/fa6";
-import { IoIosCloseCircleOutline } from "react-icons/io";
-import { IoCalendarNumberSharp, IoHome, IoMail } from "react-icons/io5";
+import { IoCalendarNumberSharp, IoCloseCircleSharp, IoHome, IoMail } from "react-icons/io5";
 import { MdMessage } from "react-icons/md";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { NavLink, Outlet } from "react-router";
@@ -16,11 +15,12 @@ const Dashboard = () => {
 
        return (
               <div className="flex">
-                     {open ? <div className="w-64 min-h-screen bg-[#D1A054] relative">
-                            <div className="font-cinzel pl-7 pt-7">
-                                   <h2 className="text-2xl font-semibold">BISTRO BOSS</h2>
-                                   <p className="tracking-[0.40em]">Restaurant</p>
-                                   <IoIosCloseCircleOutline onClick={handleHamburgerMenu} className="absolute top-10 right-5 text-3xl" />
+                     {open ? <div className="w-44 lg:w-52 min-h-screen bg-[#D1A054]">
+                            <div className="font-cinzel pl-5 pt-7">
+                                   <div>
+                                          <h2 className="text-md lg:text-xl font-semibold flex items-center">BISTRO B<span><IoCloseCircleSharp onClick={handleHamburgerMenu} className="text-xl lg:text-2xl" /></span>SS</h2>
+                                          <p className="lg:tracking-[0.30em]">Restaurant</p>
+                                   </div>
                             </div>
                             <ul className="menu">
                                    <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel flex items-center gap-2 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/dashboard/userHome'><IoHome className="text-xl" />User Home</NavLink></li>
@@ -36,23 +36,23 @@ const Dashboard = () => {
                                    <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel flex items-center gap-2 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/contact'><IoMail className="text-xl"></IoMail>Contact</NavLink></li>
                             </ul>
                      </div> :
-                     <div className="w-20 min-h-screen block  bg-[#D1A054]">
-                            <RxHamburgerMenu onClick={handleHamburgerMenu} className="text-xl mt-5 block mx-auto"></RxHamburgerMenu>
-                            <ul className="menu flex justify-center items-center">
-                                   <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/dashboard/userHome'><IoHome className="text-xl" /></NavLink></li>
-                                   <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/dashboard/reservation'><FaCalendarAlt className="text-xl" /></NavLink></li>
-                                   <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/dashboard/reservation'><FaWallet className="text-xl" /></NavLink></li>
-                                   <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/dashboard/cart'><FaShoppingCart className="text-xl"></FaShoppingCart></NavLink></li>
-                                   <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/dashboard/addReview'><MdMessage className="text-xl"></MdMessage></NavLink></li>
-                                   <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/dashboard/myBooking'><IoCalendarNumberSharp className="text-xl"></IoCalendarNumberSharp></NavLink></li>
-                                   <div className="divider"></div>
-                                   <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/'><IoHome className="text-xl"></IoHome></NavLink></li>
-                                   <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/ourMenu'><RxHamburgerMenu className="text-xl"></RxHamburgerMenu></NavLink></li>
-                                   <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/orders/Salad'><FaBagShopping className="text-xl"></FaBagShopping></NavLink></li>
-                                   <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/contact'><IoMail className="text-xl"></IoMail></NavLink></li>
-                            </ul>
-                     </div>} 
-                     <div className="flex-1">
+                            <div className="w-10 lg:w-20 min-h-screen  bg-[#D1A054]">
+                                   <RxHamburgerMenu onClick={handleHamburgerMenu} className="text-xl mt-5 block mx-auto"></RxHamburgerMenu>
+                                   <ul className="menu flex justify-center items-center">
+                                          <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/dashboard/userHome'><IoHome className="text-xl" /></NavLink></li>
+                                          <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/dashboard/reservation'><FaCalendarAlt className="text-xl" /></NavLink></li>
+                                          <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/dashboard/reservation'><FaWallet className="text-xl" /></NavLink></li>
+                                          <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/dashboard/cart'><FaShoppingCart className="text-xl"></FaShoppingCart></NavLink></li>
+                                          <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/dashboard/addReview'><MdMessage className="text-xl"></MdMessage></NavLink></li>
+                                          <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/dashboard/myBooking'><IoCalendarNumberSharp className="text-xl"></IoCalendarNumberSharp></NavLink></li>
+                                          <div className="divider"></div>
+                                          <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/'><IoHome className="text-xl"></IoHome></NavLink></li>
+                                          <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/ourMenu'><RxHamburgerMenu className="text-xl"></RxHamburgerMenu></NavLink></li>
+                                          <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/orders/Salad'><FaBagShopping className="text-xl"></FaBagShopping></NavLink></li>
+                                          <li><NavLink className={({ isActive }) => `!bg-transparent font-cinzel p-0 pb-4 ${isActive ? 'font-bold text-white' : 'text-black'}`} to='/contact'><IoMail className="text-xl"></IoMail></NavLink></li>
+                                   </ul>
+                            </div>}
+                     <div className="flex-1  bg-[#F6F6F6]">
                             <Outlet></Outlet>
                      </div>
               </div>
--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -2,13 +2,15 @@ import { FaTrashAlt } from "react-icons/fa";
 import useCart from "../../../Hooks/useCart";
 import Swal from "sweetalert2";
 import useAxios from "../../../Hooks/useAxios";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 const Cart = () => {
 
-       const [cart, refetch] = useCart()
-       const axiosSecure = useAxios()
-       const totalPrice = cart.reduce((sum, item) => sum + item.price, 0)
-       const handleDelete = id => {
+       const [cart, refetch] = useCart();
+       const axiosSecure = useAxios();
+       const totalPrice = parseFloat(cart.reduce((sum, item) => sum + item.price, 0).toFixed(2));
+
+       const handleDelete = (id) => {
               Swal.fire({
                      title: "Are you sure?",
                      text: "You won't be able to revert this!",
@@ -16,69 +18,65 @@ const Cart = () => {
                      showCancelButton: true,
                      confirmButtonColor: "#3085d6",
                      cancelButtonColor: "#d33",
-                     confirmButtonText: "Yes, delete it!"
+                     confirmButtonText: "Yes, delete it!",
               }).then((result) => {
                      if (result.isConfirmed) {
-
-                            axiosSecure.delete(`/cart/${id}`)
-                                   .then(res => {
-                                          if (res.data.deletedCount === 1) {
-                                                 Swal.fire({
-                                                        title: "Deleted!",
-                                                        text: "Your file has been deleted.",
-                                                        icon: "success"
-                                                 });
-                                                 refetch()
-                                          }
-                                   })
+                            axiosSecure.delete(`/cart/${id}`).then((res) => {
+                                   if (res.data.deletedCount === 1) {
+                                          Swal.fire({
+                                                 title: "Deleted!",
+                                                 text: "Your item has been deleted.",
+                                                 icon: "success",
+                                          });
+                                          refetch();
+                                   }
+                            });
                      }
               });
-       }
+       };
 
        return (
               <div>
-                     <div className="flex justify-between">
-                            <h2>Total Orders: {cart.length}</h2>
-                            <h2>Total Price: ${totalPrice}</h2>
-                            <button>Pay</button>
-                     </div>
-                     <div className="overflow-x-auto">
-                            <table className="table">
-                                   <thead>
-                                          <tr>
-                                                 <th>#</th>
-                                                 <th>Item Image</th>
-                                                 <th>Item Name</th>
-                                                 <th>Item Price</th>
-                                                 <th>Action</th>
-                                          </tr>
-                                   </thead>
-                                   <tbody>
-                                          {
-                                                 cart.map((item, index) => <tr key={item._id}>
-                                                        <th>{index + 1}</th>
-                                                        <td>
-                                                               <div className="flex items-center gap-3">
-                                                                      <div className="avatar">
-                                                                             <div className="mask mask-squircle h-12 w-12">
-                                                                                    <img
-                                                                                           src={item.image}
-                                                                                           alt="Avatar Tailwind CSS Component" />
-                                                                             </div>
-                                                                      </div>
-                                                               </div>
-                                                        </td>
-                                                        <td>{item.name}</td>
-                                                        <td>{item.price}</td>
-                                                        <th>
-                                                               <button onClick={() => handleDelete(item._id)} className="btn btn-ghost btn-lg"><FaTrashAlt></FaTrashAlt></button>
-                                                        </th>
+                     <SectionTitle subHeading="---My Cart---" heading="WANNA ADD MORE?" />
+                     <div className="bg-white mx-5 lg:w-3/4 px-3 py-5 lg:p-10  lg:mx-auto mb-12">
+                            <div>
+                                   <div className="flex justify-between items-center text-center text-xs md:text-base font-semibold pb-3">
+                                          <h2 className="font-cinzel">Total Orders: {cart.length}</h2>
+                                          <h2 className="font-cinzel">Total Price: {totalPrice}$</h2>
+                                          <div><button className="btn bg-[#D1A054] btn-sm">Pay</button></div>
+                                   </div>
+                                   <table className="w-full text-center">
+                                          <thead className="bg-[#D1A054] text-white text-xs md:text-base">
+                                                 <tr>
+                                                        <th className="p-2">Image</th>
+                                                        <th className="p-2">Item Name</th>
+                                                        <th className="p-2">Price</th>
+                                                        <th className="p-2">Action</th>
                                                  </tr>
-                                                 )
-                                          }
-
-                                   </tbody>
-                            </table>
+                                          </thead>
+                                          <tbody className="text-center">
+                                                 {cart.map((item) => (
+                                                        <tr key={item._id} className="border-b text-xs">
+                                                               <td className="p-2">
+                                                                      <div className="flex justify-center">
+                                                                             <img className="w-10 h-10 rounded-md" src={item.image} alt="" />
+                                                                      </div>
+                                                               </td>
+                                                               <td className="p-2">{item.name}</td>
+                                                               <td className="p-2">${item.price}</td>
+                                                               <td className="p-2">
+                                                                      <button
+                                                                             onClick={() => handleDelete(item._id)}
+                                                                             className="btn btn-ghost btn-sm text-red-500 hover:text-red-700"
+                                                                      >
+                                                                             <FaTrashAlt />
+                                                                      </button>
+                                                               </td>
+                                                        </tr>
+                                                 ))}
+                                          </tbody>
+                                   </table>
+                            </div>
                      </div>
               </div>
        );
